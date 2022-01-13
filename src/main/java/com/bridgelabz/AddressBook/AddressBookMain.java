0df488a193621ed6a74467ceaae7ebd25182b3e0
rvpatil1432 <rvpatil1432@gmail.com>
2022-01-13 08:20:09 +0530

    Added : Welcome message to master branch

---
+++ src/main/java/com/bridgelabz/AddressBook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.AddressBook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program..");
+
+	}
+
+}
